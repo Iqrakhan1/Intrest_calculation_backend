d2135e6118b69b7e1c89c91fae99efc32ccc5b44
Iqra khan . <ik22082002khan@gmail.com>
2024-09-26 13:29:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors"); // Import CORS package
+const app = express();
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json());
+
+app.get("/Interest", (req, res) => {
+  const principle = parseInt(req.query.principle);
+  const time = parseInt(req.query.time);
+  const rate = parseInt(req.query.rate);
+  const simpleIntrest = (principle * rate * time) / 100;
+  const total = principle + simpleIntrest;
+
+  res.send({
+    total: total,
+    simpleIntrest: simpleIntrest,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
